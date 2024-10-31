82b750c33d2c2c2bb0292d10de40316dcc7cc326
pedro-rodrigues <pedro.rodrigues@sptech.school>
2024-10-31 13:20:58 -0300

    termiar a consultoria e os desafios

--- src/main/java/school/sptech/Consultoria.java
+++ src/main/java/school/sptech/Consultoria.java
@@ -1,4 +1,110 @@
 package school.sptech;
 
+import school.sptech.especialistas.DesenvolvedorMobile;
+import school.sptech.especialistas.DesenvolvedorWeb;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Consultoria {
+    private String nome;
+    private Integer vagas;
+    private List<Desenvolvedor> desenvolvedores = new ArrayList<>();
+
+    public Consultoria() {}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getVagas() {
+        return vagas;
+    }
+
+    public void setVagas(Integer vagas) {
+        this.vagas = vagas;
+    }
+
+    public Boolean contratar(Desenvolvedor desenvolvedor) {
+        if (desenvolvedores.size() < vagas) {
+            desenvolvedores.add(desenvolvedor);
+            return true;
+        }
+        return false;
+    }
+
+    public Boolean contratarFullstack(DesenvolvedorWeb desenvolvedor) {
+        if (desenvolvedor.isFullstack() && desenvolvedores.size() < vagas) {
+            desenvolvedores.add(desenvolvedor);
+            return true;
+        }
+        return false;
+    }
+
+    public Double getTotalSalarios() {
+        return desenvolvedores.stream().mapToDouble(Desenvolvedor::calcularSalario).sum();
+    } //fazer um for
+
+    public Integer qtdDesenvolvedoresMobile() {
+        return (int) desenvolvedores.stream().filter(d -> d instanceof DesenvolvedorMobile).count();
+    }
+
+
+
+    // fazer o  buscarPorSalarioMaiorIgualQue()
+
+    //fazer o public Desenvolvedor buscarMenorSalario()
+
+    //fazer os dois desafios
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
